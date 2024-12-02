42c239d0720c900c96490a5a2f225c60e12ceb9e
Ethan Leatherwood <ethanleatherwood@mail.weber.edu>
2024-12-01 20:38:05 -0700

    Create cars.py

---
+++ .github/cars.py
@@ -0,0 +1,20 @@
+print('Ethan Leatherwood')
+
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeelp', 'Chevy', 'GMC']
+print(cars)
+print(len(cars))
+cars.append('Buick')
+print(cars)
+print(cars[3])
+cars.insert(3, 'Toyota')
+print(cars)
+cars.pop(5)
+print(cars)
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+my_array_length = len(cars)
+array_string = 'The length of my array is'
+print(array_string + str(my_array_length))
